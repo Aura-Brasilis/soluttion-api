3035dafe8c03d32da8a5da7559819718124e4b5e
Caio Brida <caio@aura-brasilis.com>
2025-02-27 18:04:31 -0300

    base project add

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { ZodError } from 'zod'
+import { env } from './env'
+
+export const app = fastify()
+
+app.register(cors, {
+  origin: '*',
+})
+
+app.setErrorHandler((error, _, res) => {
+  if (error instanceof ZodError) {
+    return res
+      .status(400)
+      .send({ msg: 'Validation error', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return res.status(500).send({ msg: 'Internal server error' })
+})
