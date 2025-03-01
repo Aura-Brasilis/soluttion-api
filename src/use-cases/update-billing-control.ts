f3f5fb8a88ebe584a20f5c423a9a52c63913bec3
Caio Brida <caio@aura-brasilis.com>
2025-03-01 07:29:07 -0300

    fix billing control use cases interfaces and classes names

--- src/use-cases/update-billing-control.ts
+++ src/use-cases/update-billing-control.ts
@@ -2,20 +2,20 @@ import { ControleFaturamento } from '@prisma/client'
 import { BillingControlRepository } from '../repositories/billing-control-repository'
 import { BillingControlFoundError } from './errors/billing-control-not-found'
 
-interface CreateBillingControlUseCaseRequest {
+interface UpdateBillingControlUseCaseRequest {
   data: ControleFaturamento
 }
 
-interface CreateBillingControlUseCaseResponse {
+interface UpdateBillingControlUseCaseResponse {
   billingControl: ControleFaturamento
 }
 
-export class CreateBillingControlUseCase {
+export class UpdateBillingControlUseCase {
   constructor(private billingControlRepository: BillingControlRepository) {}
 
   async execute({
     data,
-  }: CreateBillingControlUseCaseRequest): Promise<CreateBillingControlUseCaseResponse> {
+  }: UpdateBillingControlUseCaseRequest): Promise<UpdateBillingControlUseCaseResponse> {
     const billingControl = await this.billingControlRepository.findById(data.id)
 
     if (!billingControl) {
