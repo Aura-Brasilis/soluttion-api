56d9a69ee6feef713c11084b55ada38d70516952
Caio Brida <caio@aura-brasilis.com>
2025-03-02 17:27:09 -0300

    fix update billing control prisma repository payload data

--- src/repositories/prisma/prisma-billing-control-repository.ts
+++ src/repositories/prisma/prisma-billing-control-repository.ts
@@ -30,11 +30,41 @@ export class PrismaBillingControlRepository
   }
 
   async update(data: ControleFaturamento) {
+    const dataToUpdate = {
+      id_inquilino: data.id_inquilino,
+      id_usina: data.id_usina,
+      contrib_custeio_ip_cip: data.contrib_custeio_ip_cip,
+      cpfl_inquilino: data.cpfl_inquilino,
+      cred_adc_band_tarifaria: data.cred_adc_band_tarifaria,
+      credito_debito: data.credito_debito,
+      economia: data.economia,
+      incentivo_inquilino_investidor: data.incentivo_inquilino_investidor,
+      inquilino_pagar: data.inquilino_pagar,
+      investidor_receber: data.investidor_receber,
+      kwh_ativo: data.kwh_ativo,
+      kwh_injetado: data.kwh_injetado,
+      kwh_minimo: data.kwh_minimo,
+      leitura_1: data.leitura_1,
+      leitura_2: data.leitura_2,
+      mes: data.mes,
+      mes_ref: data.mes_ref,
+      minimo_investidor: data.minimo_investidor,
+      observacao: data.observacao,
+      saldo_banco_anterior: data.saldo_banco_anterior,
+      saldo_banco_atual: data.saldo_banco_atual,
+      tarifa_te_fv: data.tarifa_te_fv,
+      tarifa_tusd_fv: data.tarifa_tusd_fv,
+      taxa_adm_soluttion: data.taxa_adm_soluttion,
+      total_creditado: data.total_creditado,
+      total_tarifas_fv: data.total_tarifas_fv,
+      mes_contrato_soluttion: data.mes_contrato_soluttion,
+    }
+
     const billingControl = await prisma.controleFaturamento.update({
       where: {
         id: data.id,
       },
-      data,
+      data: dataToUpdate,
     })
 
     return billingControl
