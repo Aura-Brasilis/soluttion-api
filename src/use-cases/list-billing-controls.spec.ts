dc2d81f4b3245522a8415cec75e94c58d6ed009f
Caio Brida <caio@aura-brasilis.com>
2025-03-04 13:06:46 -0300

    add pagination in billing control list all use case and add tests

--- src/use-cases/list-billing-controls.spec.ts
+++ src/use-cases/list-billing-controls.spec.ts
@@ -74,8 +74,103 @@ describe('List billing controls use case', () => {
       total_tarifas_fv: null,
     })
 
-    const { billingControls } = await sut.execute()
+    const { billingControls } = await sut.execute({
+      pagination: { limit: 10, orderBy: '', page: 1, orderColumn: 'id' },
+    })
 
-    expect(billingControls).toHaveLength(2)
+    expect(billingControls.data).toHaveLength(2)
+  })
+
+  it('should be able to get paginated billing controls', async () => {
+    for (let i = 0; i <= 22; i++) {
+      billingControlRepository.items.push({
+        id: i,
+        contrib_custeio_ip_cip: null,
+        cpfl_inquilino: null,
+        cred_adc_band_tarifaria: null,
+        credito_debito: null,
+        economia: null,
+        id_usina: null,
+        id_inquilino: null,
+        incentivo_inquilino_investidor: null,
+        inquilino_pagar: null,
+        investidor_receber: null,
+        kwh_ativo: null,
+        kwh_injetado: null,
+        kwh_minimo: null,
+        leitura_1: 'test',
+        leitura_2: 'test',
+        mes: 'test',
+        mes_contrato_soluttion: 'test',
+        mes_ref: 'test',
+        minimo_investidor: null,
+        observacao: 'test',
+        saldo_banco_anterior: null,
+        saldo_banco_atual: null,
+        tarifa_te_fv: null,
+        tarifa_tusd_fv: null,
+        taxa_adm_soluttion: null,
+        total_creditado: null,
+        total_tarifas_fv: null,
+      })
+    }
+
+    const { billingControls } = await sut.execute({
+      pagination: { limit: 10, orderBy: '', page: 3, orderColumn: 'id' },
+    })
+
+    expect(billingControls.data).toHaveLength(3)
+    expect(billingControls.data[2].id).toBe(22)
+  })
+
+  it('should be able to get searched billing controls', async () => {
+    for (let i = 0; i <= 22; i++) {
+      billingControlRepository.items.push({
+        id: i,
+        contrib_custeio_ip_cip: null,
+        cpfl_inquilino: null,
+        cred_adc_band_tarifaria: null,
+        credito_debito: null,
+        economia: null,
+        id_usina: null,
+        id_inquilino: null,
+        incentivo_inquilino_investidor: null,
+        inquilino_pagar: null,
+        investidor_receber: null,
+        kwh_ativo: null,
+        kwh_injetado: null,
+        kwh_minimo: null,
+        leitura_1: 'test',
+        leitura_2: 'test',
+        mes: `Test user ${i}`,
+        mes_contrato_soluttion: 'test',
+        mes_ref: 'test',
+        minimo_investidor: null,
+        observacao: 'test',
+        saldo_banco_anterior: null,
+        saldo_banco_atual: null,
+        tarifa_te_fv: null,
+        tarifa_tusd_fv: null,
+        taxa_adm_soluttion: null,
+        total_creditado: null,
+        total_tarifas_fv: null,
+      })
+    }
+
+    const { billingControls } = await sut.execute({
+      pagination: {
+        limit: 10,
+        orderBy: '',
+        orderColumn: 'id',
+        page: 1,
+        search: {
+          mes: 'Test user 2',
+          id: 2,
+        },
+      },
+    })
+
+    expect(billingControls.data).toHaveLength(1)
+    expect(billingControls.data[0].id).toBe(2)
   })
 })
--- src/use-cases/list-billing-controls.ts
+++ src/use-cases/list-billing-controls.ts
@@ -1,15 +1,26 @@
 import { ControleFaturamento } from '@prisma/client'
 import { BillingControlRepository } from '../repositories/billing-control-repository'
+import { Pagination } from '@/@types/pagination'
+
+interface ListBillingControlUseCaseRequest {
+  pagination: Pagination
+}
 
 interface ListBillingControlUseCaseResponse {
-  billingControls: ControleFaturamento[]
+  billingControls: {
+    data: ControleFaturamento[]
+    pagination: Record<string, string | number | boolean> | null
+  }
 }
 
 export class ListBillingControlUseCase {
   constructor(private billingControlRepository: BillingControlRepository) {}
 
-  async execute(): Promise<ListBillingControlUseCaseResponse> {
-    const billingControls = await this.billingControlRepository.findAll()
+  async execute({
+    pagination,
+  }: ListBillingControlUseCaseRequest): Promise<ListBillingControlUseCaseResponse> {
+    const billingControls =
+      await this.billingControlRepository.findAll(pagination)
 
     return { billingControls }
   }
