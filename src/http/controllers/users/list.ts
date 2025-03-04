16c0b30ab6e0b72a1f662c6745268b850b340f45
Caio Brida <caio@aura-brasilis.com>
2025-03-04 11:32:01 -0300

    add pagination in list all users route

--- src/http/controllers/users/list.ts
+++ src/http/controllers/users/list.ts
@@ -1,17 +1,39 @@
-import { UserNotFoundError } from '@/use-cases/errors/user-not-found'
 import { makeListUserUseCase } from '@/use-cases/factories/make-list-user-use-case'
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
 
 export async function list(req: FastifyRequest, res: FastifyReply) {
-  try {
-    const listUserUseCase = makeListUserUseCase()
+  const getQuerySchema = z.object({
+    page: z.coerce.number().default(1),
+    limit: z.coerce.number().default(100),
+    orderBy: z.string().default('asc'),
+    orderColumn: z.string().default('id'),
+    search: z
+      .object({
+        id: z.number().optional(),
+        id_usuario: z.string().optional(),
+        nome: z.string().optional(),
+        email: z.string().optional(),
+        telefone: z.string().optional(),
+        cpf_cnpj: z.string().optional(),
+        razao_social: z.string().optional(),
+        investimento: z.coerce.number().optional(),
+        responsavel: z.string().optional(),
+        cep: z.string().optional(),
+        tipo: z.string().optional(),
+      })
+      .optional(),
+  })
 
-    const { users } = await listUserUseCase.execute()
+  const { limit, orderBy, orderColumn, page, search } = getQuerySchema.parse(
+    req.query,
+  )
 
-    return res.status(200).send({ users })
-  } catch (err) {
-    if (err instanceof UserNotFoundError) {
-      return res.status(404).send({ msg: err.message })
-    }
-  }
+  const listUserUseCase = makeListUserUseCase()
+
+  const { users } = await listUserUseCase.execute({
+    pagination: { limit, orderBy, orderColumn, page, search },
+  })
+
+  return res.status(200).send({ users })
 }
