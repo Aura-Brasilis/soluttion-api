b18b18d32ab9e6fd585c5eeef661444afe677deb
Caio Brida <caio@aura-brasilis.com>
2025-03-04 13:05:33 -0300

    update billing control in memory repo to add pagination

--- src/repositories/in-memory/in-memory-billing-control-repository.ts
+++ src/repositories/in-memory/in-memory-billing-control-repository.ts
@@ -1,6 +1,7 @@
 import { ControleFaturamento } from '@prisma/client'
 import { BillingControlRepository } from '../billing-control-repository'
 import { randomInt } from 'node:crypto'
+import { Pagination } from '@/@types/pagination'
 
 export class InMemoryBillingControlRepository
   implements BillingControlRepository
@@ -52,8 +53,39 @@ export class InMemoryBillingControlRepository
     return billingControl
   }
 
-  async findAll() {
-    return this.items
+  async findAll(pagination: Pagination) {
+    const { page = 1, limit = 10, search } = pagination
+
+    let filteredBillingControls = this.items
+
+    if (search) {
+      filteredBillingControls = filteredBillingControls.filter((user) => {
+        return Object.entries(search).every(([key, value]) => {
+          if (!value) return true // Ignora campos não preenchidos
+          const userValue = user[key as keyof ControleFaturamento]
+          if (typeof userValue === 'string') {
+            return userValue.toLowerCase().includes(String(value).toLowerCase())
+          }
+          return userValue === value
+        })
+      })
+    }
+
+    const total = filteredBillingControls.length
+    const startIndex = (page - 1) * limit
+    const endIndex = startIndex + limit
+    const paginatedData = filteredBillingControls.slice(startIndex, endIndex)
+
+    return {
+      data: paginatedData,
+      pagination: {
+        page,
+        limit,
+        total,
+        hasNextPage: endIndex < total,
+        hasPrevPage: startIndex > 0,
+      },
+    }
   }
 
   async update(data: ControleFaturamento) {
