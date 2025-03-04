1392b2abc721c07f4350b4facafcf37ce3c2143f
Caio Brida <caio@aura-brasilis.com>
2025-03-04 11:21:13 -0300

    Update users-repository.ts

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -1,9 +1,13 @@
+import { Pagination } from '@/@types/pagination'
 import { Usuarios, Prisma } from '@prisma/client'
 
 export interface UsersRepository {
   create(data: Prisma.UsuariosCreateInput): Promise<Usuarios>
   findById(userId: number): Promise<Usuarios | null>
-  findAll(): Promise<Usuarios[]>
+  findAll(pagination: Pagination): Promise<{
+    data: Usuarios[]
+    pagination: Record<string, string | number | boolean> | null
+  }>
   update(data: Prisma.UsuariosCreateInput): Promise<Usuarios>
   delete(userId: number): Promise<boolean>
 }
