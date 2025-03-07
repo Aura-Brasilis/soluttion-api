1e0580d7ab0fe71d77a70f43bc5c4bcb84277f2d
Caio Brida <caio@aura-brasilis.com>
2025-03-07 15:46:31 -0300

    create in memory billing items repository

---
+++ src/repositories/in-memory/in-memory-billing-items.ts
@@ -0,0 +1,50 @@
+import { itens_cobranca as ItensCobranca } from '@prisma/client'
+import { BillingItemsRepository } from '../billing-items-repository'
+import { Pagination } from '@/@types/pagination'
+
+export class InMemoryBillingItemsRepository implements BillingItemsRepository {
+  public items: ItensCobranca[] = []
+
+  async findById(billingItemsId: bigint) {
+    const billingItem = this.items.find((i) => i.id === billingItemsId)
+
+    if (!billingItem) return null
+
+    return billingItem
+  }
+
+  async findAll(pagination: Pagination) {
+    const { page = 1, limit = 10, search } = pagination
+
+    let filteredBillingItems = this.items
+
+    if (search) {
+      filteredBillingItems = filteredBillingItems.filter((user) => {
+        return Object.entries(search).every(([key, value]) => {
+          if (!value) return true // Ignora campos não preenchidos
+          const userValue = user[key as keyof ItensCobranca]
+          if (typeof userValue === 'string') {
+            return userValue.toLowerCase().includes(String(value).toLowerCase())
+          }
+          return userValue === value
+        })
+      })
+    }
+
+    const total = filteredBillingItems.length
+    const startIndex = (page - 1) * limit
+    const endIndex = startIndex + limit
+    const paginatedData = filteredBillingItems.slice(startIndex, endIndex)
+
+    return {
+      data: paginatedData,
+      pagination: {
+        page,
+        limit,
+        total,
+        hasNextPage: endIndex < total,
+        hasPrevPage: startIndex > 0,
+      },
+    }
+  }
+}
