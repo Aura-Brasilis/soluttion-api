b0bcfe9139ec4ec47510f52f47127133408e7de1
Caio Brida <caio@aura-brasilis.com>
2025-04-25 12:40:32 -0300

    add pix repositories

---
+++ src/repositories/pix/pix-qrcode-repository.ts
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import { QrCodeRepository } from '../qrcode-repository'
+import { env } from '@/env'
+
+export class PixQrCodeRepository implements QrCodeRepository {
+  async create(amount: string, id: string) {
+    try {
+      const response = await axios.post(env.QRCODE_PIX_API_URL, {
+        amount,
+        reference: id,
+        key: env.PIX_KEY,
+        key_type: env.PIX_KEY_TYPE,
+        name: env.PIX_NAME,
+        city: env.PIX_CITY,
+      })
+
+      return response.data
+    } catch (err) {
+      console.error(err)
+      return null
+    }
+  }
+}
