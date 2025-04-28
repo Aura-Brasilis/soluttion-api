3479a97fe3aa8ac039f0f93f0113b44ae715c311
Caio Brida <caio@aura-brasilis.com>
2025-04-28 03:34:43 -0300

    fix pix use cases

--- src/use-cases/factories/make-generate-pix-use-case.ts
+++ src/use-cases/factories/make-generate-pix-use-case.ts
@@ -1,10 +1,12 @@
+import { PrismaQrCodeRepository } from '@/repositories/prisma/prisma-qrcode-repository'
 import { GeneratePixUseCase } from '../generate-pix'
 import { PixQrCodeRepository } from '@/repositories/pix/pix-qrcode-repository'
 
 export function makeGeneratePixUseCase() {
   const pixRepository = new PixQrCodeRepository()
+  const qrCodeRepository = new PrismaQrCodeRepository()
 
-  const useCase = new GeneratePixUseCase(pixRepository)
+  const useCase = new GeneratePixUseCase(pixRepository, qrCodeRepository)
 
   return useCase
 }
--- src/use-cases/generate-pix.ts
+++ src/use-cases/generate-pix.ts
@@ -1,22 +1,60 @@
 import { PixQrCodeRepository } from '@/repositories/pix/pix-qrcode-repository'
+import { QrCodeRepository } from '@/repositories/qrcode-repository'
+import { pix_qrcodes as PixQrCodes, Prisma } from '@prisma/client'
 interface GeneratePixUseCaseRequest {
   amount: string
   id: string
 }
 
 interface GeneratePixUseCaseResponse {
-  data: Record<string, string | boolean | number>
+  data: PixQrCodes
 }
 
 export class GeneratePixUseCase {
-  constructor(private pixQrCodeRepository: PixQrCodeRepository) {}
+  constructor(
+    private pixQrCodeRepository: PixQrCodeRepository,
+    private qrCodeRepository: QrCodeRepository,
+  ) {}
 
   async execute({
     amount,
     id,
   }: GeneratePixUseCaseRequest): Promise<GeneratePixUseCaseResponse> {
-    const data = await this.pixQrCodeRepository.create(amount, id)
+    const pixFounded = await this.qrCodeRepository.findById(id)
 
-    return { data }
+    const renewPixMinutes = 30
+
+    const nowDate = new Date()
+    const renewDate = new Date(nowDate.getTime() + renewPixMinutes * 60 * 1000)
+
+    if (!pixFounded || (pixFounded && nowDate > pixFounded.renew_at)) {
+      const data = await this.pixQrCodeRepository.create(amount, id)
+
+      const payload: Prisma.pix_qrcodesCreateInput = {
+        key: data.key as string,
+        created_at: nowDate,
+        renew_at: renewDate,
+        key_type: data.key_type as string,
+        name: data.name as string,
+        city: data.city as string,
+        amount: data.amount as string,
+        code: data.code as string,
+        formated_amount: data.formated_amount as string,
+        qrcode_base64: data.qrcode_base64 as string,
+        faturas_energia: {
+          connect: {
+            fatura_numero: data.reference as string,
+          },
+        },
+      }
+
+      const pix = pixFounded
+        ? await this.qrCodeRepository.update(id, payload)
+        : await this.qrCodeRepository.create(payload)
+
+      return { data: pix }
+    }
+
+    return { data: pixFounded }
   }
 }
